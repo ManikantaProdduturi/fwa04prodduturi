0b96fc6b81ac7badc7e6a8b564874ce2449f23f9
ManikantaProdduturi <mani.uf90@gmail.com>
2021-10-27 21:19:51 -0500

    update everything

--- routes/computation.js
+++ routes/computation.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var value=Math.round(Math.random()*100);
-
-var s= Math.hypot(value)  ;
-var t=Math.ceil(value) 
-var h=Math.clz32(value)
- 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
- 
- res.send('the function [Math.hypot() applied to] ['+ value+'] is ['+s+']\nthe function [Math.ceil() applied to] ['+ value+'] is ['+t+']\nthe function [Math.clz32() applied to] ['+ value+'] is ['+h+']');
-
+  let result=""
+  if(Object.entries(req.query).length === 0){
+    let rand = Math.random() * 100
+    res.render('computation', { result: `Math.cos applied to ${rand} is ${Math.cos(rand)}` });
+  
+} else{
+  for (const key in req.query) {
+    console.log(req.query)
+    result+= `Math.cos applied to ${req.query[key]} is ${Math.cos(req.query[key])} `
+  }
+  res.render('computation', { result: result});
+}
 });
 
 module.exports = router;
